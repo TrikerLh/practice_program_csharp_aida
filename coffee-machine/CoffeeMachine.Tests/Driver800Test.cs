fef05662ff1bbf251f34d5f4fec77f7f4ac4e349
Rufino Perez <ruperez@aidacanarias.com>
2024-04-12 11:18:17 +0200

    green test

---
+++ coffee-machine/CoffeeMachine.Tests/Driver800Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CoffeeMachine.Tests
+{
+    public class Driver800Test
+    {
+        [Test]
+        public void Serve_Chocolate()
+        {
+            DrinkMaker drinkMaker = Substitute.For<DrinkMaker>();
+            Order order = new Order(DrinkType.Chocolate);
+            Driver800 driver800 = new Driver800(drinkMaker);
+
+            driver800.Send(order);
+
+            drinkMaker.Received().Execute("C::");
+        }
+    }
+}
---
+++ coffee-machine/CoffeeMachine/Driver800.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+    public class Driver800 : DrinkMakerDriver
+    {
+        private DrinkMaker drinkMaker;
+
+        public Driver800(DrinkMaker drinkMaker)
+        {
+            this.drinkMaker = drinkMaker;
+        }
+
+        public void Send(Order order)
+        {
+            drinkMaker.Execute("C::");
+        }
+    }
+}
