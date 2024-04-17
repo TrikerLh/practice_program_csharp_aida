6cfb6c22f0f055d9ee3678160addbcf471ef8340
Lorenzo <llanuza@aidacanarias.com>
2024-04-17 09:56:26 +0100

    test: Get_Select_Drink_Message_In_England

--- coffee-machine-c-alt/CoffeeMachineApp.Tests/core/MessageComposerManuallyTest.cs
+++ coffee-machine-c-alt/CoffeeMachineApp.Tests/core/MessageComposerManuallyTest.cs
@@ -1,4 +1,5 @@
-﻿using CoffeeMachineApp.core;
+﻿using System.Globalization;
+using CoffeeMachineApp.core;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -11,14 +12,26 @@ public class MessageComposerManuallyTest
     [SetUp]
     public void Setup()
     {
-        _messageComposer = new MessageComposerManually();
+       
     }
 
     [Test]
     public void Get_Select_Drink_Message_In_Spain()
     {
+        _messageComposer = new MessageComposerManually(new CultureInfo("es-ES"));
+
         var result = _messageComposer.ComposeSelectDrinkMessage();
 
         result.Should().Be(Message.Create("¡Por favor, seleccione bebida!"));
+    }
+
+    [Test]
+    public void Get_Select_Drink_Message_In_England()
+    {
+        _messageComposer = new MessageComposerManually(new CultureInfo("en-GB"));
+
+        var result = _messageComposer.ComposeSelectDrinkMessage();
+
+        result.Should().Be(Message.Create("Please, select drink!"));
     }
 }
--- coffee-machine-c-alt/CoffeeMachineApp/core/MessageComposerManually.cs
+++ coffee-machine-c-alt/CoffeeMachineApp/core/MessageComposerManually.cs
@@ -1,7 +1,23 @@
-﻿namespace CoffeeMachineApp.core;
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace CoffeeMachineApp.core;
 
 public class MessageComposerManually : MessageComposer
 {
+    private IDictionary<CultureInfo, string> _selectDrinkMessages = new Dictionary<CultureInfo, string>()
+    {
+        { new CultureInfo("es-ES"), "¡Por favor, seleccione bebida!"}
+        , { new CultureInfo("en-GB"), "Please, select drink!"}
+    };
+
+    private readonly CultureInfo _currentCultureInfo;
+
+    public MessageComposerManually(CultureInfo currentCultureInfo)
+    {
+        _currentCultureInfo = currentCultureInfo;
+    }
+
     public Message ComposeMissingMoneyMessage(decimal missingPrice)
     {
         return default;
@@ -9,6 +25,6 @@ public class MessageComposerManually : MessageComposer
 
     public Message ComposeSelectDrinkMessage()
     {
-        return Message.Create("¡Por favor, seleccione bebida!");
+        return Message.Create(_selectDrinkMessages[_currentCultureInfo]);
     }
 }
