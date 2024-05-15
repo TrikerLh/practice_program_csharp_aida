9914f9564634350db53c61b5633b04c8a5dbc18b
Raúl Corchero <corchero100@gmail.com>
2024-05-15 12:57:57 +0100

    Implements message error when product is not available

--- shopping-cart/ShoppingCart.Tests/ShoppingCartTest.cs
+++ shopping-cart/ShoppingCart.Tests/ShoppingCartTest.cs
@@ -1,3 +1,5 @@
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using NUnit.Framework;
 
 namespace ShoppingCart.Tests
@@ -5,9 +7,62 @@ namespace ShoppingCart.Tests
     public class ShoppingCartTest
     {
         [Test]
-        public void Canary_Test()
+        public void Display_error_when_product_not_available()
         {
-            Assert.That(true, Is.True);
+            var productRepository = Substitute.For<ProductRepository>();
+            var discountRepository = Substitute.For<DiscountRepository>();
+            var checkoutService = Substitute.For<CheckoutService>();
+            var display = Substitute.For<Display>();
+            var shoppingCart = new ShoppingCart(productRepository, discountRepository, checkoutService, display);
+            var productNoAvailable = "IceCream";
+            productRepository.Get(productNoAvailable).ReturnsNull();
+
+            shoppingCart.AddItem(productNoAvailable);
+
+            display.Received(1).Show("Producto no disponible.");
         }
+    }
+
+    public class ShoppingCart
+    {
+        private readonly ProductRepository _productRepository;
+        private readonly DiscountRepository _discountRepository;
+        private readonly CheckoutService _checkoutService;
+        private readonly Display _display;
+
+        public ShoppingCart(ProductRepository productRepository, DiscountRepository discountRepository, CheckoutService checkoutService, Display display)
+        {
+            _productRepository = productRepository;
+            _discountRepository = discountRepository;
+            _checkoutService = checkoutService;
+            _display = display;
+        }
+
+        public void AddItem(string productName)
+        {
+               _display.Show("Producto no disponible.");
+        }
+    }
+
+    public interface Display
+    {
+        void Show(string message);
+    }
+
+    public interface CheckoutService
+    {
+    }
+
+    public interface DiscountRepository
+    {
+    }
+
+    public interface ProductRepository
+    {
+        Product Get(string productName);
+    }
+
+    public class Product
+    {
     }
 }
