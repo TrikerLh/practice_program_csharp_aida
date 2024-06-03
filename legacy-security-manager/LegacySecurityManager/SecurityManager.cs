659703398719aba5a52014ab966a46ef91be5ca8
Raúl Corchero <rcorchero@aidacanarias.com>
2024-06-03 16:38:46 +0100

    Refactor

--- legacy-security-manager/LegacySecurityManager/SecurityManager.cs
+++ legacy-security-manager/LegacySecurityManager/SecurityManager.cs
@@ -4,6 +4,8 @@ namespace LegacySecurityManager;
 
 public class SecurityManager
 {
+    private const int MAX_PASSWORD_LENGTH = 8;
+
     public void CreateSecurityUser()
     {
         Print("Enter a username");
@@ -15,23 +17,35 @@ public class SecurityManager
         Print("Re-enter your password");
         var confirmPassword = Read();
 
+        if (IsInvalidPassword(password, confirmPassword)) return;
+
+        var array = EncryptPassword(password);
+
+        Print($"Saving Details for User ({username}, {fullName}, {new string(array)})\n");
+    }
+
+    private char[] EncryptPassword(string password)
+    {
+        char[] array = password.ToCharArray();
+        Array.Reverse(array);
+        return array;
+    }
+
+    private bool IsInvalidPassword(string password, string confirmPassword)
+    {
         if (password != confirmPassword)
         {
             Print("The passwords don't match");
-            return;
+            return true;
         }
 
-        if (password.Length < 8)
+        if (password.Length < MAX_PASSWORD_LENGTH)
         {
             Print("Password must be at least 8 characters in length");
-            return;
+            return true;
         }
 
-        // Encrypt the password (just reverse it, should be secure)
-        char[] array = password.ToCharArray();
-        Array.Reverse(array);
-
-        Print($"Saving Details for User ({username}, {fullName}, {new string(array)})\n");
+        return false;
     }
 
     protected virtual string Read()
