e634375e54bde99206dd096c6be29e8df13a7ae2
Raúl Corchero <rcorchero@aidacanarias.com>
2024-06-03 16:25:55 +0100

    test: Fail_When_Confirmed_Password_is_different_Than_Password

--- legacy-security-manager/LegacySecurityManager.Tests/SecurityManagerTest.cs
+++ legacy-security-manager/LegacySecurityManager.Tests/SecurityManagerTest.cs
@@ -5,9 +5,36 @@ namespace LegacySecurityManager.Tests
     public class SecurityManagerTest
     {
         [Test]
-        public void Canary_Test()
+        public void Fail_When_Confirmed_Password_is_different_Than_Password()
         {
-            Assert.That(true, Is.True);
+            var securityManager = new SecurityManagerForTesting(new List<string> { "UserName","Full Name", "Password1", "Password2" });
+
+            securityManager.CreateSecurityUser();
+
+            Assert.That(securityManager.messages, Has.Some.EquivalentTo("The passwords don't match"));
+
+        }
+    }
+
+    public class SecurityManagerForTesting : SecurityManager
+    {
+        private readonly List<string> _inputs;
+        private int _count;
+        public List<string> messages;
+        public SecurityManagerForTesting(List<string> inputs)
+        {
+            _inputs = inputs;
+            _count = 0;
+            messages = new List<string>();
+        }
+        protected override string Read()
+        {
+            return _inputs[_count++];
+        }
+
+        protected override void Print(string message)
+        {
+            messages.Add(message);
         }
     }
 }
--- legacy-security-manager/LegacySecurityManager/SecurityManager.cs
+++ legacy-security-manager/LegacySecurityManager/SecurityManager.cs
@@ -4,26 +4,26 @@ namespace LegacySecurityManager;
 
 public class SecurityManager
 {
-    public static void CreateUser()
+    public void CreateSecurityUser()
     {
-        Console.WriteLine("Enter a username");
-        var username = Console.ReadLine();
-        Console.WriteLine("Enter your full name");
-        var fullName = Console.ReadLine();
-        Console.WriteLine("Enter your password");
-        var password = Console.ReadLine();
-        Console.WriteLine("Re-enter your password");
-        var confirmPassword = Console.ReadLine();
+        Print("Enter a username");
+        var username = Read();
+        Print("Enter your full name");
+        var fullName = Read();
+        Print("Enter your password");
+        var password = Read();
+        Print("Re-enter your password");
+        var confirmPassword = Read();
 
         if (password != confirmPassword)
         {
-            Console.WriteLine("The passwords don't match");
+            Print("The passwords don't match");
             return;
         }
 
         if (password.Length < 8)
         {
-            Console.WriteLine("Password must be at least 8 characters in length");
+            Print("Password must be at least 8 characters in length");
             return;
         }
 
@@ -31,6 +31,21 @@ public class SecurityManager
         char[] array = password.ToCharArray();
         Array.Reverse(array);
 
-        Console.WriteLine("Saving Details for User ({0}, {1}, {2})\n", username, fullName, new string(array));
+        Print($"Saving Details for User ({username}, {fullName}, {new string(array)})\n");
+    }
+
+    protected virtual string Read()
+    {
+        return Console.ReadLine();
+    }
+
+    protected virtual void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void CreateUser()
+    {
+        new SecurityManager().CreateSecurityUser();
     }
 }
