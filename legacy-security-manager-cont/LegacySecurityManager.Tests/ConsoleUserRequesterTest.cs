4924654e393c326b83ca609c067d53ea2c6ce886
Carlos <carlosinguanzo.ci@gmail.com>
2024-06-06 13:37:29 +0200

    Test: ✅ Request_User_Data

---
+++ legacy-security-manager-cont/LegacySecurityManager.Tests/ConsoleUserRequesterTest.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace LegacySecurityManager.Tests
+{
+    public class ConsoleUserRequesterTest
+    {
+        [Test]
+        public void Request_User_Data()
+        {
+            var inputRequester = Substitute.For<InputRequester>();
+            var consoleUserDataRequester = new ConsoleUserDataRequester(inputRequester);
+            var name = "Andrea";
+            var fullName = "Perez";
+            var password = "Andrea123";
+            inputRequester.RequestInput("Enter a username").Returns(name);
+            inputRequester.RequestInput("Enter your full name").Returns(fullName);
+            inputRequester.RequestInput("Enter your password").Returns(password);
+            inputRequester.RequestInput("Re-enter your password").Returns(password);
+
+            var userData = consoleUserDataRequester.Request();
+
+            var userDataExpected = new UserData(name,fullName,password,password);
+            Assert.That(userData, Is.EqualTo(userDataExpected));
+        }
+    }
+}
--- legacy-security-manager-cont/LegacySecurityManager.Tests/SecurityManagerTest.cs
+++ legacy-security-manager-cont/LegacySecurityManager.Tests/SecurityManagerTest.cs
@@ -1,77 +1,59 @@
-using NSubstitute;
+﻿using NSubstitute;
 using NUnit.Framework;
 
-namespace LegacySecurityManager.Tests;
-
-public class SecurityManagerTest
+namespace LegacySecurityManager.Tests
 {
-    private const string Username = "Pepe";
-    private const string FullName = "Pepe Garcia";
-    private Notifier _notifier;
-    private InputReader _reader;
-    private SecurityManager _securityManager;
-
-    [SetUp]
-    public void Setup()
+    public class SecurityManagerTest
     {
-        _notifier = Substitute.For<Notifier>();
-        _reader = Substitute.For<InputReader>();
-        UserDataRequester requester = new ConsoleUserDataRequester(new ConsoleInputRequester(_reader, _notifier));
-        _securityManager = new SecurityManager(_notifier, requester);
-    }
+        private const string Username = "Pepe";
+        private const string FullName = "Pepe Garcia";
+        private Notifier _notifier;
+        private UserDataRequester _requester;
+        private SecurityManager _securityManager;
 
-    [Test]
-    public void do_not_save_user_when_password_and_confirm_password_are_not_equals()
-    {
-        IntroducingPasswords("Pepe1234", "Pepe1234.");
-
-        CreateUser();
-
-        VerifyNotifiedMessages("The passwords don't match");
-    }
-
-    [Test]
-    public void do_not_save_user_when_password_too_short()
-    {
-        IntroducingPasswords("Pepe123", "Pepe123");
-
-        CreateUser();
-
-        VerifyNotifiedMessages("Password must be at least 8 characters in length");
-    }
-
-    [Test]
-    public void save_user()
-    {
-        var validPassword = "Pepe1234";
-        IntroducingPasswords(validPassword, validPassword);
-
-        CreateUser();
-
-        var reversedPassword = "4321epeP";
-        VerifyNotifiedMessages($"Saving Details for User ({Username}, {FullName}, {reversedPassword})\n");
-    }
-
-    private void CreateUser()
-    {
-        _securityManager.CreateValidUser();
-    }
-
-    private void IntroducingPasswords(string password, string confirmedPassword)
-    {
-        _reader.Read().Returns(Username, FullName, password, confirmedPassword);
-    }
-
-    private void VerifyNotifiedMessages(string lastMessage)
-    {
-        Received.InOrder(() =>
+        [SetUp]
+        public void Setup()
         {
-            _notifier.Received(1).Notify("Enter a username");
-            _notifier.Received(1).Notify("Enter your full name");
-            _notifier.Received(1).Notify("Enter your password");
-            _notifier.Received(1).Notify("Re-enter your password");
-            _notifier.Received(1).Notify(lastMessage);
-        });
-        _notifier.Received(5).Notify(Arg.Any<string>());
+            _notifier = Substitute.For<Notifier>();
+            _requester = Substitute.For<UserDataRequester>();
+            _securityManager = new SecurityManager(_notifier, _requester);
+        }
+
+        [Test]
+        public void do_not_save_user_when_password_and_confirm_password_are_not_equals()
+        {
+            _requester.Request().Returns(CreateUserData("Pepe1234", "Pepe1234."));
+
+            _securityManager.CreateValidUser();
+
+            _notifier.Received(1).Notify("The passwords don't match");
+        }
+
+        [Test]
+        public void do_not_save_user_when_password_too_short()
+        {
+            _requester.Request().Returns(CreateUserData("Pepe123", "Pepe123"));
+
+            _securityManager.CreateValidUser();
+
+            _notifier.Received(1).Notify("Password must be at least 8 characters in length");
+        }
+
+        [Test]
+        public void save_user()
+        {
+            var validPassword = "Pepe1234";
+            _requester.Request().Returns(CreateUserData(validPassword, validPassword));
+
+            _securityManager.CreateValidUser();
+
+            var reversedPassword = "4321epeP";
+            _notifier.Received(1).Notify($"Saving Details for User ({Username}, {FullName}, {reversedPassword})\n");
+        }
+
+        private static UserData CreateUserData(string password, string confirmPassword)
+        {
+            return new UserData(Username, FullName, password, confirmPassword);
+        }
     }
 }
