d9bfcd6c03b789132e72d479df908d09a9860be4
Raúl Corchero <rcorchero@aidacanarias.com>
2024-06-10 10:06:05 +0100

    Converto to internal ConsoleUserDataRequester

--- legacy-security-manager-cont-2/LegacySecurityManager/SecurityManager.cs
+++ legacy-security-manager-cont-2/LegacySecurityManager/SecurityManager.cs
@@ -3,6 +3,8 @@ using System;
 
 namespace LegacySecurityManager;
 
+
+
 public class SecurityManager
 {
     private readonly Notifier _notifier;
@@ -77,4 +79,36 @@ public class SecurityManager
         Notifier notifier = new ConsoleNotifier();
         new SecurityManager(notifier, new ConsoleInput()).CreateValidUser();
     }
+
+    internal class ConsoleUserDataRequester {
+        private Input input;
+
+        public ConsoleUserDataRequester(Input input) {
+            this.input = input;
+        }
+        public UserData Request() {
+
+            return new UserData(RequestUserName(), RequestFullName(), RequestPassword(), RequestPasswordConfirmation());
+        }
+
+        private string RequestPasswordConfirmation() {
+            return input.Request("Re-enter your password");
+        }
+
+        private string RequestPassword() {
+            return input.Request("Enter your password");
+        }
+
+        private string RequestFullName() {
+            return input.Request("Enter your full name");
+        }
+
+        private string RequestUserName() {
+            return input.Request("Enter a username");
+        }
+
+    }
 }
+
+
+
