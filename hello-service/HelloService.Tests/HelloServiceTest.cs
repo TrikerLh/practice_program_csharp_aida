8a863764811020bb25313ef1d4c32a7e55d3f0cd
Raúl Corchero <rcorchero@aidacanarias.com>
2024-06-11 09:24:50 +0100

    implemenmts say Buenos dias! between 6AM and 11:59 AM

--- hello-service/HelloService.Tests/HelloServiceTest.cs
+++ hello-service/HelloService.Tests/HelloServiceTest.cs
@@ -1,3 +1,4 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Hello.Tests
@@ -5,9 +6,50 @@ namespace Hello.Tests
     public class HelloServiceTest
     {
         [Test]
-        public void Canary_Test()
+        public void Say_Buenos_dias_between_6AM_to_11_59_AM()
         {
-            Assert.That(true, Is.True);
+            var outputService = Substitute.For<OutputService>();
+            DateTimeProvider dateTimeProvider = Substitute.For<DateTimeProvider>();
+            dateTimeProvider.Get().Returns(new TimeOnly(6, 00));
+            HelloService helloService = new HelloService(outputService, dateTimeProvider);
+
+            helloService.Hello();
+
+            outputService.Received().Write("Buenos dias!");
         }
     }
+
+    public class HelloService
+    {
+        private readonly OutputService _outputService;
+        private readonly DateTimeProvider _dateTimeProvider;
+
+        public HelloService(OutputService outputService, DateTimeProvider dateTimeProvider)
+        {
+            _outputService = outputService;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public void Hello()
+        {
+            _outputService.Write("Buenos dias!");
+        }
+    }
+
+    public interface DateTimeProvider
+    {
+        public TimeOnly Get();
+    }
+
+    public interface OutputService
+    {
+        public string Write(string message);
+    }
 }
+
+
+/* Lista de Test
+ * Te saluda con "Buenos días!" desde las 6:00 AM hasta las 11:59AM
+ * Te saluda con "Buenas tardes!" desde las 12:00 PM hasta las 07:59 PM
+ * Te saluda con "Buenas noches!" desde las 08:00 PM hasta las 5:59 AM
+ * */
