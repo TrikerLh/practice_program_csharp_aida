ec7afe47a2f5a8ab1ebbfa97afad47750a3bcbac
Raúl Corchero <corchero100@gmail.com>
2024-06-15 22:13:21 +0100

    Refactor: move code to class SummaryFormatter

--- stock-broker-service/StockBroker/StockBrokerClient.cs
+++ stock-broker-service/StockBroker/StockBrokerClient.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Microsoft.VisualBasic;
 
 namespace StockBroker;
 
@@ -10,14 +9,14 @@ public class StockBrokerClient
     private readonly DateTimeProvider _dateTimeProvider;
     private readonly Notifier _notifier;
     private readonly StockBrokerService _stockBrokerService;
-    private static CultureInfo _cultureInfo;
+    private readonly SummaryFormatter _summaryFormatter;
 
     public StockBrokerClient(DateTimeProvider dateTimeProvider, Notifier notifier, StockBrokerService stockBrokerService)
     {
         _dateTimeProvider = dateTimeProvider;
         _notifier = notifier;
         _stockBrokerService = stockBrokerService;
-        _cultureInfo = new CultureInfo("en-US");
+        _summaryFormatter = new SummaryFormatter(new CultureInfo("en-US"));
     }
 
     public void PlaceOrder(string orderSequence)
@@ -32,7 +31,7 @@ public class StockBrokerClient
                 orderFail += order.GetSymbol();
             }
         }
-        var summary = GetFormatSummary(time, orders, orderFail);
+        var summary = _summaryFormatter.GetFormatSummary(time, orders, orderFail);
         _notifier.Notify(summary);
     }
     private List<Order> GetOrder(string orderSequence) {
@@ -46,29 +45,33 @@ public class StockBrokerClient
         {
             var symbol = order.Split(" ")[0];
             var quantity = int.Parse(order.Split(" ")[1]);
-            var price = double.Parse(order.Split(" ")[2], _cultureInfo);
+            var price = double.Parse(order.Split(" ")[2], CultureInfo.InvariantCulture);
             var type = order.Split(" ")[3];
             orders.Add(new Order(symbol, quantity, price, type));
         }
         return orders;
     }
+}
 
-    //TODO: es static, podría ir en una clase formater?
+internal class SummaryFormatter
+{
+    private readonly CultureInfo _cultureInfo;
 
-    private static string GetFormatSummary(DateTime time, IList<Order> orders, string orderFail)
+    public SummaryFormatter(CultureInfo cultureInfo)
     {
+        _cultureInfo = cultureInfo;
+    }
+
+    internal string GetFormatSummary(DateTime time, IList<Order> orders, string orderFail) {
         var timeFormated = time.ToString("g", _cultureInfo);
-        if (orders.Count == 0)
-        {
+        if (orders.Count == 0) {
             return timeFormated + " Buy: € 0.00, Sell: € 0.00";
         }
 
-        if (string.IsNullOrEmpty(orderFail))
-        {
+        if (string.IsNullOrEmpty(orderFail)) {
             var buyAmount = 0.0;
             var sellAmount = 0.0;
-            foreach (var order in orders)
-            {
+            foreach (var order in orders) {
                 buyAmount += order.GetBuyAmount();
                 sellAmount += order.GetSellAmount();
             }
@@ -77,7 +80,7 @@ public class StockBrokerClient
         return timeFormated + " Buy: € 0.00, Sell: € 0.00, Failed: " + orderFail;
     }
 
-    private static string FormatAmount(double amount) {
+    private string FormatAmount(double amount) {
         return "€ " + amount.ToString("F2", _cultureInfo);
     }
 }
