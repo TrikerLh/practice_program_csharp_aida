69ecd4bcd26f0b7b95cbb6ed403a0e13f2c0526d
Ramón Martínez <ramartinez@aidacanarias.com>
2024-06-18 13:14:19 +0200

    extract method in suite tests

--- checkout-with-side-effects/Checkout.Tests/CheckoutTest.cs
+++ checkout-with-side-effects/Checkout.Tests/CheckoutTest.cs
@@ -12,14 +12,17 @@ public class CheckoutTest
         var amount = new Money(12);
         var receipt = checkout.CreateReceipt(amount);
 
-        Assert.That(amount, Is.EqualTo(receipt.Amount));
-        Assert.That(new Money(2.4m), Is.EqualTo(receipt.Tax));
-        Assert.That(new Money(14.4m), Is.EqualTo(receipt.Total));
+        CheckReceipt(receipt, 12, 2.4m, 14.4m);
 
         Assert.That(1, Is.EqualTo(checkout.StoredReceipts.Count));
-        Assert.That(amount, Is.EqualTo(checkout.StoredReceipts[0].Amount));
-        Assert.That(new Money(2.4m), Is.EqualTo(checkout.StoredReceipts[0].Tax));
-        Assert.That(new Money(14.4m), Is.EqualTo(checkout.StoredReceipts[0].Total));
+        CheckReceipt(checkout.StoredReceipts[0], 12, 2.4m, 14.4m);
+    }
+
+    private static void CheckReceipt(Receipt receipt, decimal amount, decimal tax, decimal total)
+    {
+        Assert.That(new Money(amount), Is.EqualTo(receipt.Amount));
+        Assert.That(new Money(tax), Is.EqualTo(receipt.Tax));
+        Assert.That(new Money(total), Is.EqualTo(receipt.Total));
     }
 }
 
