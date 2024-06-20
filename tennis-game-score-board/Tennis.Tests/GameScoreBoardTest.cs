cc2e53d556704757c9cb5b1bcfb6deab030bef6a
Antonio Dovao <adovao@aidacanarias.com>
2024-06-20 09:33:01 +0100

    ♻️ Refactor tests

--- tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
+++ tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
@@ -5,16 +5,25 @@ namespace Tennis.Tests;
 
 public class GameScoreBoardTest
 {
+    private InputScore _inputScore;
+    private OutputMessage _outputMessage;
+    private GameScoreBoard _gameScoreBoard;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _inputScore = Substitute.For<InputScore>();
+        _outputMessage = Substitute.For<OutputMessage>();
+        _gameScoreBoard = new GameScoreBoard(_inputScore, _outputMessage);
+    }
+
     [Test]
     public void player_one_score()
     {
-        var inputScore = Substitute.For<InputScore>();
-        inputScore.ReadScore().Returns("score 1");
-        var outputMessage = Substitute.For<OutputMessage>();
-        var gameScoreBoard = new GameScoreBoard(inputScore, outputMessage);
+        _inputScore.ReadScore().Returns("score 1");
 
-        gameScoreBoard.StartGame();
+        _gameScoreBoard.StartGame();
 
-        outputMessage.Received(1).Send("Fifteen Love");
+        _outputMessage.Received(1).Send("Fifteen Love");
     }
 }
