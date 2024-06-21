a2b56fc741ee07422de9400881ec0f1c407fd2dc
Raúl Corchero <corchero100@gmail.com>
2024-06-21 23:37:15 +0100

    refactor

--- tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
+++ tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
@@ -71,8 +71,6 @@ public class GameScoreBoardTest
         Deuce(1);
         Advantage("2", 1);
         Won("2");
-        _outputMessage.Received(1).Send("It was a nice game.");
-        _outputMessage.Received(1).Send("Bye now!");
     }
 
 
@@ -99,14 +97,47 @@ public class GameScoreBoardTest
 
         _gameScoreBoard.StartGame();
 
-        _outputMessage.Received(1).Send("Fifteen Love");
-        _outputMessage.Received(1).Send("Fifteen Fifteen");
-        _outputMessage.Received(1).Send("Thirty Fifteen");
-        _outputMessage.Received(1).Send("Thirty Thirty");
-        _outputMessage.Received(1).Send("Forty Thirty");
-        _outputMessage.Received(3).Send("Deuce");
-        _outputMessage.Received(3).Send("Advantage player 1");
-        _outputMessage.Received(1).Send("Player 1 has won!!");
+        FifteenLove();
+        FifteenFifteen();
+        ThirtyFifteen();
+        ThirtyThirty();
+        FortyThirty();
+        Deuce(3);
+        Advantage("1", 3);
+        Won("1");
+        _outputMessage.Received(14).Send(Arg.Any<string>());
+    }
+
+    private void ThirtyForty()
+    {
+        _outputMessage.Received(1).Send("Thirty Forty");
+    }
+
+
+    private void FifteenThirty()
+
+    {
+        _outputMessage.Received(1).Send("Fifteen Thirty");
+    }
+
+    private void LoveForty()
+    {
+        _outputMessage.Received(1).Send("Love Forty");
+    }
+
+    private void LoveThirty()
+    {
+        _outputMessage.Received(1).Send("Love Thirty");
+    }
+
+    private void LoveFifteen()
+    {
+        _outputMessage.Received(1).Send("Love Fifteen");
+    }
+
+    private void Won(string player)
+    {
+        _outputMessage.Received(1).Send($"Player {player} has won!!");
         _outputMessage.Received(1).Send("It was a nice game.");
         _outputMessage.Received(1).Send("Bye now!");
     }
