b6fb6ddf27aec1928b44f1e50d605f0ba2897262
Raúl Corchero <corchero100@gmail.com>
2024-06-21 23:31:50 +0100

    Refactor test

--- tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
+++ tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
@@ -24,12 +24,10 @@ public class GameScoreBoardTest
 
         _gameScoreBoard.StartGame();
 
-        _outputMessage.Received(1).Send("Fifteen Love");
-        _outputMessage.Received(1).Send("Thirty Love");
-        _outputMessage.Received(1).Send("Forty Love");
-        _outputMessage.Received(1).Send("Player 1 has won!!");
-        _outputMessage.Received(1).Send("It was a nice game.");
-        _outputMessage.Received(1).Send("Bye now!");
+        FifteenLove();
+        ThirtyLove();
+        FortyLove();
+        Won("1");
     }
 
     [Test]
@@ -39,12 +37,10 @@ public class GameScoreBoardTest
 
         _gameScoreBoard.StartGame();
 
-        _outputMessage.Received(1).Send("Love Fifteen");
-        _outputMessage.Received(1).Send("Love Thirty");
-        _outputMessage.Received(1).Send("Love Forty");
-        _outputMessage.Received(1).Send("Player 2 has won!!");
-        _outputMessage.Received(1).Send("It was a nice game.");
-        _outputMessage.Received(1).Send("Bye now!");
+        LoveFifteen();
+        LoveThirty();
+        LoveForty();
+        Won("2");
     }
 
     [Test]
@@ -67,16 +63,14 @@ public class GameScoreBoardTest
 
         _gameScoreBoard.StartGame();
 
-        _outputMessage.Received(1).Send("Love Fifteen");
-        _outputMessage.Received(1).Send("Fifteen Fifteen");
-        _outputMessage.Received(1).Send("Fifteen Thirty");
-        _outputMessage.Received(1).Send("Thirty Thirty");
-        _outputMessage.Received(1).Send("Thirty Forty");
-        _outputMessage.Received(1).Send("Deuce");
-        _outputMessage.Received(1).Send("Advantage player 2");
-        _outputMessage.Received(1).Send("Player 2 has won!!");
-        _outputMessage.Received(1).Send("It was a nice game.");
-        _outputMessage.Received(1).Send("Bye now!");
+        LoveFifteen();
+        FifteenFifteen();
+        FiftennTherty();
+        ThirtyThirty();
+        ThirtyForty();
+        Deuce(1);
+        Advantage("2", 1);
+        Won("2");
     }
 
 
@@ -86,16 +80,14 @@ public class GameScoreBoardTest
 
         _gameScoreBoard.StartGame();
 
-        _outputMessage.Received(1).Send("Fifteen Love");
-        _outputMessage.Received(1).Send("Fifteen Fifteen");
-        _outputMessage.Received(1).Send("Thirty Fifteen");
-        _outputMessage.Received(1).Send("Thirty Thirty");
-        _outputMessage.Received(1).Send("Forty Thirty");
-        _outputMessage.Received(1).Send("Deuce");
-        _outputMessage.Received(1).Send("Advantage player 1");
-        _outputMessage.Received(1).Send("Player 1 has won!!");
-        _outputMessage.Received(1).Send("It was a nice game.");
-        _outputMessage.Received(1).Send("Bye now!");
+        FifteenLove();
+        FifteenFifteen();
+        ThirtyFifteen();
+        ThirtyThirty();
+        FortyThirty();
+        Deuce(1);
+        Advantage("1", 1);
+        Won("1");
         _outputMessage.Received(10).Send(Arg.Any<string>());
     }
 
@@ -105,16 +97,91 @@ public class GameScoreBoardTest
 
         _gameScoreBoard.StartGame();
 
-        _outputMessage.Received(1).Send("Fifteen Love");
-        _outputMessage.Received(1).Send("Fifteen Fifteen");
-        _outputMessage.Received(1).Send("Thirty Fifteen");
-        _outputMessage.Received(1).Send("Thirty Thirty");
-        _outputMessage.Received(1).Send("Forty Thirty");
-        _outputMessage.Received(3).Send("Deuce");
-        _outputMessage.Received(3).Send("Advantage player 1");
-        _outputMessage.Received(1).Send("Player 1 has won!!");
+        FifteenLove();
+        FifteenFifteen();
+        ThirtyFifteen();
+        ThirtyThirty();
+        FortyThirty();
+        Deuce(3);
+        Advantage("1", 3);
+        Won("1");
+        _outputMessage.Received(14).Send(Arg.Any<string>());
+    }
+
+    private void ThirtyForty()
+    {
+        _outputMessage.Received(1).Send("Thirty Forty");
+    }
+
+    private void FiftennTherty()
+    {
+        _outputMessage.Received(1).Send("Fifteen Thirty");
+    }
+
+    private void LoveForty()
+    {
+        _outputMessage.Received(1).Send("Love Forty");
+    }
+
+    private void LoveThirty()
+    {
+        _outputMessage.Received(1).Send("Love Thirty");
+    }
+
+    private void LoveFifteen()
+    {
+        _outputMessage.Received(1).Send("Love Fifteen");
+    }
+
+    private void Won(string player)
+    {
+        _outputMessage.Received(1).Send($"Player {player} has won!!");
         _outputMessage.Received(1).Send("It was a nice game.");
         _outputMessage.Received(1).Send("Bye now!");
-        _outputMessage.Received(14).Send(Arg.Any<string>());
+    }
+
+    private void Advantage(string player, int requiredNumberOfCalls)
+    {
+        _outputMessage.Received(requiredNumberOfCalls).Send($"Advantage player {player}");
+    }
+
+    private void Deuce(int requiredNumberOfCalls)
+    {
+        _outputMessage.Received(requiredNumberOfCalls).Send("Deuce");
+    }
+
+    private void FortyLove()
+    {
+        _outputMessage.Received(1).Send("Forty Love");
+    }
+
+    private void ThirtyLove()
+    {
+        _outputMessage.Received(1).Send("Thirty Love");
+    }
+
+    private void FortyThirty()
+    {
+        _outputMessage.Received(1).Send("Forty Thirty");
+    }
+
+    private void ThirtyThirty()
+    {
+        _outputMessage.Received(1).Send("Thirty Thirty");
+    }
+
+    private void ThirtyFifteen()
+    {
+        _outputMessage.Received(1).Send("Thirty Fifteen");
+    }
+
+    private void FifteenFifteen()
+    {
+        _outputMessage.Received(1).Send("Fifteen Fifteen");
+    }
+
+    private void FifteenLove()
+    {
+        _outputMessage.Received(1).Send("Fifteen Love");
     }
 }
